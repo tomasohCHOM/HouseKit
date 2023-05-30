ad38731a68465a7ada460bd8cfc390ca588d2bd5
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-05-30 13:01:09 -0700

    Started implementing username and password form fill

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* To handle the Main Page Navbar */
 const openNavbar = document.getElementById('open-navbar-menu');
 const closeNavbar = document.getElementById('close-navbar-menu');
 const navbar = document.getElementById('navbar');
@@ -13,3 +14,22 @@ closeNavbar.addEventListener('click', () => {
 function handleNavbarMenu() {
     navbar.classList.toggle('open-navbar');
 }
+
+/* To handle User Login when they do not input a Username or Password */
+const loginWrapper = document.getElementById('login-wrapper');
+const usernameInput = document.getElementById('login-username');
+const passwordInput = document.getElementById('login-password');
+const loginSubmit = document.getElementById('login-submit');
+
+loginSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let errorMessage = "";
+    if (usernameInput.value == "" || usernameInput.value == null) {
+        errorMessage = "Please input a correct username.";
+        const errorElement = document.body.createElement('p');
+        errorElement.innerHTML = errorMessage;
+        loginWrapper.appendChild(errorElement);
+    } else if (passwordInput.value == "" || passwordInput.value == null) {
+        errorMessage = "Please input a correct password.";
+    }
+});
