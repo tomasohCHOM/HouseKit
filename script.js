43e781e32ba950535bf09286b1ab15e2900cf9a8
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-05-31 11:00:24 -0700

    Handled navbar inside script rather than repeating it multiple times in HTML

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-/* To handle the Main Page Navbar */
-const openNavbar = document.getElementById('open-navbar-menu');
-const closeNavbar = document.getElementById('close-navbar-menu');
-const navbar = document.getElementById('navbar');
+/* Load the navbar as the window is loaded */
+const navbar = document.getElementById("navbar");
+window.addEventListener("DOMContentLoaded", () => {
+  navbar.innerHTML = `<header class="navbar-header">
+                            <button id="close-navbar-menu" class="js-close-navbar close-navbar-button">
+                                <ion-icon name="close-outline"></ion-icon>
+                            </button>
+                            <img onclick="window.location.href='index.html'" class="housekit-logo-navbar" src="images/header-images/housekit-logo.png">
+                        </header>
+                        <ul>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="person-circle-outline"></ion-icon>My Account</a>
+                            </li>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="cart-outline"></ion-icon>My Cart</a></li>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="home-outline"></ion-icon>Furnitures</a></li>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="pricetags-outline"></ion-icon>Latest Offers</a></li>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="information-circle-outline"></ion-icon>About Us</a></li>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="people-outline"></ion-icon>Customer Service</a></li>
+                            <li>
+                                <a href=#><ion-icon class="navbar-icons" name="call-outline"></ion-icon>Contact Us</a></li>
+                            <button onclick="window.location.href='login.html'" class="sign-in-button navbar-sign-in">
+                                Sign In
+                            </button>
+                        </ul>`;
+  /* To handle the Main Page Navbar */
+  const openNavbar = document.getElementById("open-navbar-menu");
+  const closeNavbar = document.getElementById("close-navbar-menu");
 
-openNavbar.addEventListener('click', () => {
+  openNavbar.addEventListener("click", () => {
     handleNavbarMenu();
-});
+  });
 
-closeNavbar.addEventListener('click', () => {
+  closeNavbar.addEventListener("click", () => {
     handleNavbarMenu();
-});
+  });
 
-function handleNavbarMenu() {
-    navbar.classList.toggle('open-navbar');
-}
+  function handleNavbarMenu() {
+    navbar.classList.toggle("open-navbar");
+  }
+});
 
 /* To handle User Login when they do not input a Username or Password */
-const loginWrapper = document.getElementById('login-wrapper');
-const usernameInput = document.getElementById('login-username');
-const passwordInput = document.getElementById('login-password');
-const loginSubmit = document.getElementById('login-submit');
-const loginErrorMessage = document.getElementById('login-error-message');
+const loginWrapper = document.getElementById("login-wrapper");
+const usernameInput = document.getElementById("login-username");
+const passwordInput = document.getElementById("login-password");
+const loginSubmit = document.getElementById("login-submit");
+const loginErrorMessage = document.getElementById("login-error-message");
 
-loginSubmit.addEventListener('click', (e) => {
+/* ONLY do this action if we are in the login page */
+if (loginSubmit) {
+  loginSubmit.addEventListener("click", (e) => {
     e.preventDefault();
     let errorMessage = "";
     if (usernameInput.value == "" || usernameInput.value == null) {
-        errorMessage = "Please input a correct username.";
+      errorMessage = "Please input a correct username.";
     } else if (passwordInput.value == "" || passwordInput.value == null) {
-        errorMessage = "Please input a correct password.";
+      errorMessage = "Please input a correct password.";
     } else {
-        if (passwordInput.value.length < 6 || passwordInput.value.length > 20) {
-            errorMessage = "Password must be between 6 and 20 characters.";
-        }
+      if (passwordInput.value.length < 6 || passwordInput.value.length > 20) {
+        errorMessage = "Password must be between 6 and 20 characters.";
+      }
     }
     loginErrorMessage.textContent = errorMessage;
-});
+  });
+}
